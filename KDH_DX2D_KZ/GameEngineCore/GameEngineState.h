258d707650a59a493329334072bedbb00b038390
DahyunKim <risehyun01@gmail.com>
2023-10-19 19:26:02 +0900

    [ADD/1019] 엔진 기능 추가 : 스테이트

---
+++ KDH_DX2D_KZ/GameEngineCore/GameEngineState.h
@@ -0,0 +1,97 @@
+#pragma once
+
+
+class CreateStateParameter
+{
+public:
+	// 진짜 맴버변수를 가질수는 없다.
+	std::function<void(class GameEngineState* _Parent)> Init;
+	std::function<void(class GameEngineState* _Parent)> Start;
+	std::function<void(float DeltaTime, class GameEngineState* _Parent)> Stay;
+	std::function<void(class GameEngineState* _Parent)> End;
+};
+
+class State
+{
+public:
+	std::string Name;
+
+	CreateStateParameter Event;
+};
+
+// 설명 :
+class GameEngineState
+{
+public:
+	template<typename EnumType>
+	void ChangeState(EnumType _Stateindex)
+	{
+		return ChangeState(static_cast<int>(_Stateindex));
+	}
+
+	void ChangeState(int _Stateindex)
+	{
+		if (nullptr != CurState && nullptr != CurState->Event.End)
+		{
+			CurState->Event.End(this);
+		}
+
+		CurState = &States[_Stateindex];
+
+		if (nullptr != CurState && nullptr != CurState->Event.Start)
+		{
+			CurState->Event.Start(this);
+			StateTime = 0.0f;
+		}
+	}
+
+	template<typename EnumType>
+	void CreateState(EnumType _Stateindex, const CreateStateParameter& _Para)
+	{
+		return CreateState(static_cast<int>(_Stateindex), _Para);
+	}
+
+	void CreateState(int _Stateindex, const CreateStateParameter& _Para)
+	{
+		States[_Stateindex].Event = _Para;
+	}
+
+	template<typename EnumType>
+	void Find(EnumType _Stateindex)
+	{
+		return Find(static_cast<int>(_Stateindex));
+	}
+
+	State* Find(int _Index)
+	{
+		if (false == States.contains(_Index))
+		{
+			return nullptr;
+		}
+		return &States[_Index];
+	}
+
+	void Update(float _DeltaTime)
+	{
+		StateTime += _DeltaTime;
+
+		if (nullptr == CurState || nullptr == CurState->Event.Stay)
+		{
+			return;
+		}
+
+		CurState->Event.Stay(_DeltaTime, this);
+	}
+
+	float GetStateTime()
+	{
+		return StateTime;
+	}
+
+protected:
+
+private:
+	float StateTime = 0.0f;
+	State* CurState = nullptr;
+	std::map<int, State> States;
+};
