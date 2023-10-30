fac69143ab5e670bdcffecd50ccb2f089b603906
risehyun <risehyun01@gmail.com>
2023-10-30 23:32:26 +0900

    [UPDATE/1030] Boss 리소스 업데이트

--- KDH_DX2D_KZ/GameEngineContents/Boss.cpp
+++ KDH_DX2D_KZ/GameEngineContents/Boss.cpp
@@ -15,20 +15,40 @@ void Boss::DirCheck()
 
 void Boss::Start()
 {
+
+	{
+		GameEnginePath FilePath;
+		FilePath.SetCurrentPath();
+		FilePath.MoveParentToExistsChild("ContentsResources");
+		FilePath.MoveChild("ContentsResources\\Texture\\Boss\\");
+
+		GameEngineTexture::Load(FilePath.PlusFilePath("Boss_ExplosionAreaCircle.png"));
+		GameEngineSprite::CreateSingle("Boss_ExplosionAreaCircle.png");
+
+
+		GameEngineTexture::Load(FilePath.PlusFilePath("Boss_RifleAttackLine.png"));
+		GameEngineSprite::CreateSingle("Boss_RifleAttackLine.png");
+
+	}
+
+
+
 	BossMainRenderer = CreateComponent<GameEngineSpriteRenderer>(static_cast<int>(ContentsRenderType::Play));
 	BossMainRenderer->AutoSpriteSizeOn();
 
 	BossMainRenderer->CreateAnimation("Idle", "spr_headhunter_idle");
 	BossMainRenderer->CreateAnimation("PutBackRifle", "spr_headhunter_putbackrifle", 0.1f, 0, 7, false);
 	BossMainRenderer->CreateAnimation("TakeOutRifle", "spr_headhunter_takeoutrifle", 0.1f, 0, 7, false);
-
+	BossMainRenderer->CreateAnimation("Shoot", "spr_headhunter_shoot", 0.1f, 0, 7, false);
+	BossMainRenderer->CreateAnimation("PutBackGun", "spr_headhunter_putbackgun", 0.1f, 0, 6, false);
+	
+	BossMainRenderer->ChangeAnimation("PutBackGun");
 	
 //	BossMainRenderer->CreateAnimation("Death", "spr_gangsterhurtground", 0.2f, 0, 5, false);
 //	BossMainRenderer->CreateAnimation("Turn", "spr_gangsterturn", 0.2f, 0, 5, false);
 
 	
 
-	BossMainRenderer->ChangeAnimation("TakeOutRifle");
 
 
 
@@ -38,7 +58,7 @@ void Boss::Start()
 	BossEffectRenderer->Transform.SetLocalPosition({ -560.0f, 14.0f });
 	BossEffectRenderer->CreateAnimation("BossLaser", "Boss_Laser");
 	BossEffectRenderer->ChangeAnimation("BossLaser");
-
+	BossEffectRenderer->Off();
 }
 
 void Boss::Update(float _Delta)
