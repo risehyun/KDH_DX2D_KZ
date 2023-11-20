40dc551f254a0c0a5b250665c152790db6f0ac11
risehyun <risehyun01@gmail.com>
2023-11-20 16:49:58 +0900

    [TEST/1120] Roll Dust 테스트

--- KDH_DX2D_KZ/GameEngineContents/FX_DustCloudGroup.cpp
+++ KDH_DX2D_KZ/GameEngineContents/FX_DustCloudGroup.cpp
@@ -21,8 +21,7 @@ void FX_DustCloudGroup::SetType(EFX_DustCloudGroup_Type _Type)
 	}
 	else if (Type == EFX_DustCloudGroup_Type::RollCloud)
 	{
-		IsStartRollCloudMove = true;
-		
+		CreateRollCloudGroup();
 	}
 }
 
@@ -40,13 +39,25 @@ void FX_DustCloudGroup::Update(float _Delta)
 		// 다시 플레이어 위치를 실시간으로 갱신
 		PlayerPos = Player::MainPlayer->Transform.GetLocalPosition();
 		float4 FxGroupPos = Transform.GetLocalPosition();
-		float4 NextPos = PlayerPos - FxGroupPos;
+
+		float initXPos = 30.0f;
+		float initYPos = 0.0f;
 
 		int a = DustCloudGroup.size();
 
 		for (int i = 0; i < DustCloudGroup.size(); i++)
 		{
-			DustCloudGroup[i]->Transform.AddLocalPosition(NextPos * _Delta * Speed);
+			if (i == 1)
+			{
+				initYPos = 10.0f;
+			}
+
+			else if (i == 2)
+			{
+				initYPos = -10.0f;
+			}
+
+			DustCloudGroup[i]->Transform.SetLocalPosition({ initXPos + PlayerPos.X + (i * 25), initYPos + PlayerPos.Y - 30.0f });
 		}
 
 		
@@ -196,24 +207,112 @@ void FX_DustCloudGroup::CreateRollCloudGroup()
 		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
 		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
 
-		float XPos = 40.0f;
+		float XPos = -10.0f;
 		float XPosDouble = XPos * 2.0f;
 
 		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
 
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -30.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
 
 		DustCloudGroup.push_back(NewFx);
 
-		/*NewFx->Transform.AddLocalPosition(NextPos);*/
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
 
-		//if (float4::RIGHT == PlayerDir)
-		//{
-		//	NewFx->Transform.SetLocalPosition({ FxInitPos.X - XPosDouble, FxInitPos.Y });
-		//}
-		//else
-		//{
-		//	NewFx->Transform.SetLocalPosition(FxInitPos);
-		//}
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -50.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -70.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -90.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -110.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -130.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
+	}
+
+	{
+		std::shared_ptr<Fx> NewFx = GetLevel()->CreateActor<Fx>();
+		NewFx->SetFxData(EFx_Type::DustCloud, -PlayerDir);
+
+		float XPos = -150.0f;
+		float XPosDouble = XPos * 2.0f;
+
+		float4 FxInitPos = { PlayerPos.X + XPos,  PlayerPos.Y - 30.0f };
+
+		DustCloudGroup.push_back(NewFx);
+
+		NewFx->Transform.SetLocalPosition(FxInitPos);
 	}
 
 	IsStartRollCloudMove = true;
